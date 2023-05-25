eea91f7cf536d5e2d8bfc615d00d7286bc6915f3
hoangmgh <109824355+hoangmgh@users.noreply.github.com>
2023-05-24 21:54:46 -0400

    Update extract_barcodes_from_rna.py

--- docker/demultiplexing/souporcell/extract_barcodes_from_rna.py
+++ docker/demultiplexing/souporcell/extract_barcodes_from_rna.py
@@ -9,5 +9,6 @@ if len(argv) != 4:
 
 data = io.read_input(argv[1])
 data.filter_data(min_genes=int(argv[3]))
+print(data.obs_names[0:10])
 with open(argv[2], "w") as fout:
         fout.write('\n'.join([i.split("-")[1]+"-"+i.split("-")[0] for i in data.obs_names]) + '\n')
